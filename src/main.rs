dee7b192cce96cd6f372b6c6ca191055d5c29aeb
Hashaam Zafar <hashaam1217@gmail.com>
2024-02-18 22:11:22 -0800

    Accept ls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdin, stdout, Write};
+use std::process::Command;
+
+fn main(){
+    loop {
+        // use the `>` character as the prompt
+        // need to explicitly flush this to ensure it prints before read_line
+        print!("> ");
+        //Ignoring the result of stdout().flush()
+        let _ = stdout().flush();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let command = input.trim();
+
+        let mut child = Command::new(command)
+            .spawn()
+            .unwrap();
+
+        // don't accept another command until this one completes
+        //Ignoring the result of child.wait()
+        let _ = child.wait();
+    }
 }
